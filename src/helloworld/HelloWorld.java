0f27def87fc2e96112dd0a6f20ee4c659effc096
ridhimananda <nandari@sheridancollege.ca>
2019-09-17 17:36:09 -0700

    testing local repository

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+
+package helloworld;
+
+/**
+ *
+ * @author Ridhima Nanda
+ */
+public class HelloWorld 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        HelloWorld describe=new HelloWorld();
+        System.out.println(describe);
+    }
+    public String toString()
+    {
+        return" I am instance of class HelloWorld!";
+    }
+    
+}
